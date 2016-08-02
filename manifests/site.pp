2460337aef49d0e26636c145c4f8fa36400f5297
paradb <bry666@gmail.com>
2016-08-02 17:06:30 -0400

    Update hellloooo

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
+  notify { "Hellllllo, my name is ${::hostname}": }
   
   
   package {'cowsay':
