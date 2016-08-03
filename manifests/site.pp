b3dcec929a4e338591e43f1f42de329380fa2feb
paradb <bry666@gmail.com>
2016-08-03 14:08:09 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "${::fqdn} has no node definition": }
   notify { "Hello from the Default Node Section: My name is ${::hostname}": }
 
 
