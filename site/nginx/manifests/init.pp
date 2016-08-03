2e923f64dd51cd93fb554fd2137dda4e5c2bd846
paradb <bry666@gmail.com>
2016-08-03 15:49:35 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,37 +4,7 @@ package { 'nginx':
 ensure =>present,
 }
 
-file {'/var/www':
-ensure => directory,
-owner => 'root',
-group => 'root',
-}
 
-file { '/var/www/index.html':
-ensure => file,
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet///modules/nginx/index.html',
-}
-
-file { 'default.conf':
-ensure => file,
-path => 'etc/nginx/conf.d/default.conf',
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet///modules/nginx/default.conf',
-}
-
-file { 'nginx.conf':
-ensure => file,
-path => 'etc/nginx/nginx.conf',
-owner => 'root',
-group => 'root',
-mode => '0644',
-source => 'puppet///modules/nginx/nginx.conf',
-}
 
 
 service {'nginx':
