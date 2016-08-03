6605e1b4b44deff521f65b97d8e3b470cf82dd3e
paradb <bry666@gmail.com>
2016-08-03 15:28:04 -0400

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,46 @@
+class nginx {
+
+package { 'nginx':
+ensure =>present,
+}
+
+file {'/var/www':
+ensure => directory,
+owner => 'root',
+group => 'root',
+}
+
+file { '/var/www/index.html':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet///modules/nginx/index.html',
+}
+
+file { 'default.conf':
+ensure => file,
+path => 'etc/nginx/conf.d/default.conf',
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet///modules/nginx/default.conf',
+}
+
+file { 'nginx.conf':
+ensure => file,
+path => 'etc/nginx/nginx.conf',
+owner => 'root',
+group => 'root',
+mode => '0644',
+source => 'puppet///modules/nginx/nginx.conf',
+}
+
+
+service {'nginx':
+ensure =>running,
+enable =>true,
+subscribe => [File['default.conf'],File['nginx.conf']]
+}
+
+}
