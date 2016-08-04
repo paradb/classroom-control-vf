baf0a44b0d571122c9c70b5f7f8711b7be01631b
paradb <bry666@gmail.com>
2016-08-04 13:18:24 -0400

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -5,3 +5,16 @@ ensure => present,
 }
 
 
+define users::managed_user (
+ $group = $title,
+ ) {
+ user { $title:
+ ensure  => present,
+ }
+ file { "/home/${title}":
+ ensure => directory,
+ owner  => $title,
+ group  => $group,
+}
+}
+
