d5b6c1b1efe779ccdcc232d2d59dc7cf3039d0c3
paradb <bry666@gmail.com>
2016-08-04 09:50:31 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -18,27 +18,18 @@ class nginx {
   
   file { '/var/www/index.html':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'default.conf':
     ensure  => file,
     path    => '/etc/nginx/conf.d/default.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/default.conf',
   }
   
   file { 'nginx.conf':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     source  => 'puppet:///modules/nginx/nginx.conf',
   }
   
